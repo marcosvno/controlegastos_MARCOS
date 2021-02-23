e7bca5bdc2e007d40f34bb88d8da743120cfa82a
marcosvno <marcos.noberto@escolar.ifrn.edu.br>
2021-02-23 14:33:20 -0300

    Criação dos controllers(Gastos, Categorias e suas views)

---
+++ application/views/listar_gastos.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <td>Data</td>
+            <td>Descrição</td>
+            <td>Valor</td>
+        </tr>
+        <tr>
+            <td>23/02/2021</td>
+            <td>Transporte</td>
+            <td>R$ 4,50</td>
+        </tr>
+        <tr>
+            <td>23/02/2021</td>
+            <td>Alimentação</td>
+            <td>R$ 11</td>
+        </tr>
+        <tr>
+            <td>23/02/2021</td>
+            <td>Livros</td>
+            <td>R$ 22,99</td>
+        </tr>
+    </table>
+</body>
+</html>
---
+++ application/views/novo_gasto.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form>
+        Data: <input type="date" name="data"><br>
+        Descrição: <input type="text" name="descricao"><br>
+        Valor: <input type="number" name="valor"><br>
+
+        <button type="submit">Enviar</button>
+        <button type="reset">Limpar</button>
+    </form>
+</body>
+</html>
